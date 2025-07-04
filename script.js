b02ba217db5494a292b9ca1d2b8303bb9badff9a
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-07-04 13:27:45 +0200

    Feedback edit changed

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 
 const images = [
-  "img/lion1.avif", "img/lion2.avif", "img/lion3.avif", "img/lion4.avif",
-  "img/lion5.avif", "img/lion6.avif", "img/lion7.avif", "img/lion8.avif",
-  "img/lion9.avif", "img/lion10.avif", "img/lion11.avif", "img/lion12.avif"
+  "img/lion1.avif", 
+  "img/lion2.avif", 
+  "img/lion3.avif", 
+  "img/lion4.avif",
+  "img/lion5.avif", 
+  "img/lion6.avif", 
+  "img/lion7.avif", 
+  "img/lion8.avif",
+  "img/lion9.avif", 
+  "img/lion10.avif", 
+  "img/lion11.avif", 
+  "img/lion12.avif"
 ];
 
 let currentIndex = 0;
 
-function renderImages() {
-  const container = document.getElementById("imageContainer");
-  container.innerHTML = "";
 
-  images.forEach((src, index) => {
-    const div = document.createElement("div");
-    div.className = "image-tile";
-    div.onclick = () => showOverlay(index);
-    div.innerHTML = `<img src="${src}" alt="Lion ${index + 1}" />`;
-    container.appendChild(div);
-  });
+ function renderImages() {
+  const container = document.getElementById("imageContainer");
+  let html = "";
+
+  for (let i = 0; i < images.length; i++) {
+    html += `
+      <div class="image-tile" onclick="showOverlay(${i})">
+        <img src="${images[i]}" alt="Lion ${i + 1}" />
+      </div>
+    `;
+  }
+
+  container.innerHTML = html;
 }
+
 
 function showOverlay(index) {
   currentIndex = index;
@@ -36,16 +49,14 @@ function toggleOverlay() {
 
 function prevImage(event) {
   event.stopPropagation();
-  if (currentIndex > 0) {
-    currentIndex--;
-    showOverlay(currentIndex);
-  }
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showOverlay(currentIndex);
 }
+
 
 function nextImage(event) {
   event.stopPropagation();
-  if (currentIndex < images.length - 1) {
-    currentIndex++;
-    showOverlay(currentIndex);
-  }
+  currentIndex = (currentIndex + 1) % images.length;
+  showOverlay(currentIndex);
 }
+
